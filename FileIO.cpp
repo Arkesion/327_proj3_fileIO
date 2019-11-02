b2142db1cd6230a7be46bfa76f638c03addbe28e
Kevin Quirici <kevin.quirici.16@cnu.edu>
2019-11-01 21:34:54 -0400

    100 in empty and small, 75 in full

--- FileIO.cpp
+++ FileIO.cpp
@@ -2,7 +2,7 @@
  * FileReader.cpp
  *
  *  Created on: Oct 8, 2017
- *      Author: keith
+ *      Author: Kevin Quirici
  */
 #include <iostream>
 #include <fstream>
@@ -10,15 +10,39 @@
 #include "../327_proj3_test/includes/constants.h"
 
 using namespace std;
+using namespace KP_FileIO;
 
 int KP_FileIO::getFileContents(const std::string &filename, std::string &contents)
 {
-	//TODO fill in
+	ifstream myFile;
+	myFile.open(filename);
+
+	if (!myFile.is_open())
+		return COULD_NOT_OPEN_FILE_TO_READ;
+
+	string line;
+
+	while (getline(myFile, line))
+		contents += line;
+
+	myFile.close();
+	return SUCCESS;
 }
 
 int KP_FileIO::writeVectortoFile(const std::string filename,std::vector<std::string> &myEntryVector)
 {
-	//TODO fill in
+	ofstream myFile;
+	myFile.open(filename);
+
+	if (!myFile.is_open())
+		return COULD_NOT_OPEN_FILE_TO_WRITE;
+
+	vector<string>::iterator vectorIterator;
+
+	for (vectorIterator = myEntryVector.begin(); vectorIterator != myEntryVector.end(); vectorIterator++)
+		myFile << *vectorIterator + "\n";
+
+	return SUCCESS;
 }
 
 
