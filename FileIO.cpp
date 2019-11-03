04d9bfb4774bf85694c360aa72c0e44cfd0a798e
Kevin Quirici <kevin.quirici.16@cnu.edu>
2019-11-02 22:54:33 -0500

    I did the best I could, but I get a 95 running in eclipse with \\ path, 80 when running in eclipse with / path, and a 75 in terminal.

--- FileIO.cpp
+++ FileIO.cpp
@@ -15,7 +15,9 @@ using namespace KP_FileIO;
 int KP_FileIO::getFileContents(const std::string &filename, std::string &contents)
 {
 	ifstream myFile;
-	myFile.open(filename);
+	myFile.open(filename.c_str());
+
+	std::cout << "Read Path: " << filename.c_str() << std::endl;
 
 	if (!myFile.is_open())
 		return COULD_NOT_OPEN_FILE_TO_READ;
@@ -32,7 +34,9 @@ int KP_FileIO::getFileContents(const std::string &filename, std::string &content
 int KP_FileIO::writeVectortoFile(const std::string filename,std::vector<std::string> &myEntryVector)
 {
 	ofstream myFile;
-	myFile.open(filename);
+	myFile.open(filename.c_str());
+
+	std::cout << "Write Path: " << filename.c_str() << std::endl;
 
 	if (!myFile.is_open())
 		return COULD_NOT_OPEN_FILE_TO_WRITE;
